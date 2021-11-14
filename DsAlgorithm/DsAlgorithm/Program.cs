5da944b89d88a55e5fad06ebcf0b7dc36c2fa539
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-11-14 19:37:49 +0530

    Welcome to Ds Algorithm

---
+++ DsAlgorithm/DsAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DsAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to data structure algorithm");
+        }
+    }
+}
