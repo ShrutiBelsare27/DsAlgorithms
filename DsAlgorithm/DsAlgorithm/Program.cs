1953cc9897d4e64985615863ad9b63ca277d4673
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-11-15 23:49:21 +0530

    Added: customize message demonstration using string function and regex

--- DsAlgorithm/DsAlgorithm/Program.cs
+++ DsAlgorithm/DsAlgorithm/Program.cs
@@ -7,7 +7,7 @@ namespace DsAlgorithm
     {
         static void Main(string[] args)
         {
-            /*string S = "abc";
+            string S = "abc";
             int n = S.Length;
             UC1_Permutation.Permute(S, 0, n - 1);
 
@@ -24,19 +24,22 @@ namespace DsAlgorithm
             UC6_Anagram.Anagram();
 
             Console.WriteLine("\n UC7");
-            UC7_PrimeNumber.Prime();*/
+            UC7_PrimeNumber.Prime();
 
-            /*Console.WriteLine("\n UC8");
+            Console.WriteLine("\n UC8");
             UC8_AnagramAndPalindromeNumber uc8 = new UC8_AnagramAndPalindromeNumber();
-            uc8.anagramAndPalindrome();*/
+            uc8.anagramAndPalindrome();
 
             /*Console.WriteLine("\n UC9");
             UC9_SearchingAndSortingUsingGenerics obj=new UC9_SearchingAndSortingUsingGenerics();
             obj.BubbleSort<int>(1,6,3);*/
 
-            Console.WriteLine("\n UC11");
+            /*Console.WriteLine("\n UC11");
             UC11TaskDoneByMaximumAmount tdma = new UC11TaskDoneByMaximumAmount();
             tdma.tasks();
+*/
+            Console.WriteLine("\n UC12");
+            UC12_RegexReplace.Regex1();
 
         }
     
---
+++ DsAlgorithm/DsAlgorithm/UC12_RegexReplace.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DsAlgorithm
+{
+   public  class UC12_RegexReplace
+    {
+        public static void Regex1()
+        {
+            string input = "Hello <<name>>,name as << full name >> in our system.\nyour contact number is 91 - xxxxxxxxxx.\nPlease, let us know in case of any clarification Thank you BridgeLabz 01 / 01 / 2016.";
+            Console.WriteLine(input);
+
+            input = Regex.Replace(input, @"<<name>>", "Shruti");
+            input = Regex.Replace(input, @"<< full name >>", "Shruti Belsare");
+            input = Regex.Replace(input, @"9493375895", "");
+            input = Regex.Replace(input, @"01 / 01 / 2016", "11 / 11/ 2021");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Replace Details Using Regex");
+            Console.WriteLine("\n");
+            Console.WriteLine(input);
+        }
+    }
+}
